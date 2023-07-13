b7854002bd1b38f7a8a475d10ab3ec7f4d68da60
dmitrii <dmitrii@dmitriis-MacBook-Pro.local>
2023-07-13 09:06:42 -0700

    small_fixes

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,42 +1,42 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-import Form from '../../components/Form'
-import { set } from 'mongoose'
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  const [submitting, setSubmitting] = useState(false)
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: ''
-  })
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
   const createPrompt = async (e) => {
-    e.preventDefault()
-    setSubmitting(true)
+    e.preventDefault();
+    setIsSubmitting(true);
+
     try {
-      const response = await fetch('/api/prompt/new', 
-      {
-        method: 'POST',
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
         body: JSON.stringify({
           prompt: post.prompt,
-          userId: sesson?.user.id,
-          tag: post.tag
-        })
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
 
-      })
       if (response.ok) {
-        router.push('/')
+        router.push("/");
       }
-    } catch(error) {
-      console.error(error)
+    } catch (error) {
+      console.log(error);
     } finally {
-      setSubmitting(false)
+      setIsSubmitting(false);
     }
-  }
+  };
+
   return (
     <Form
       type='Create'
@@ -44,8 +44,8 @@ const CreatePrompt = () => {
       setPost={setPost}
       submitting={submitting}
       handleSubmit={createPrompt}
-      />
-  )
-}
+    />
+  );
+};
 
-export default CreatePrompt
+export default CreatePrompt;
