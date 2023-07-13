16d0dc1b4fa21f3ab6be1a1d4c3382a1c7dbbf97
dmitrii <dmitrii@dmitriis-MacBook-Pro.local>
2023-07-13 09:13:55 -0700

    checking

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,7 +5,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
-
+//sas
 const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
