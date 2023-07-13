7c7967016fb33892c95457048f245d5d54bf75d1
dmitrii <dmitrii@dmitriis-MacBook-Pro.local>
2023-07-12 17:04:14 -0700

    add_users

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,10 @@ import NextAuth from "next-auth/next";
 
 import GoogleProvider from 'next-auth/providers/google'
 
+import { connectToDB } from '../../../../utils/database'
+
+import User from '../../../../models/user'
+
 console.log({
     clientId: process.env.GOOGLE_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -19,9 +23,17 @@ const handler = NextAuth({
     },
     async signIn({profile}) {
         try {
-            
-        } catch(error) {
+            await connectToDB()
 
+            //check if user is existed
+
+            //if not, create a new user 
+
+
+            return true
+        } catch(error) {
+            console.error(error)
+            return false
         }
     }
 
