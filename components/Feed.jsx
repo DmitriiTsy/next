7253ba6a9f23d235ba8a61ec7c98aceefd60764a
dmitrii <dmitrii@dmitriis-MacBook-Pro.local>
2023-09-04 20:53:40 -0700

    delete_unusefull_text

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -42,7 +42,7 @@ const Feed = () => {
       <form className='relative w-full flex-center'>
         <input
           type='text'
-          placeholder='Search for tag or a username'
+          placeholder='Search for tag'
           value={searchText}
           onChange={handleSearchChange}
           required
